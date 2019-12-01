4a7928a4791917dd261ed6a4773a1983652f1777
Annika Rings <annika.rings@dpag.ox.ac.uk>
2019-12-01 21:49:09 +0000

    changed Comments

--- src/day1.hs
+++ src/day1.hs
@@ -4,34 +4,45 @@ module Day1
   )
   where
 
+-- partially applying divison and subtraction functions to facilitate composition later on
+
 div3 :: Int -> Int
-div3 n = div n 3
+div3 n = div n 3 -- div already rounds the result down to the next smaller integer
 
 subtract2 :: Int -> Int
 subtract2 n = n - 2
 
+-- calculating fuel
+
 fuel :: Int ->Int
 fuel = subtract2 . div3
 
-readInt :: String -> Int
-readInt = read
+
+-- for Part 2:
 
 extrafuel f
   |fuel f <= 0 = 0
   |otherwise = fuel f + extrafuel (fuel f)
 
+-- reading the input and printing the results
+
+readInt :: String -> Int
+readInt = read
+
+
+
 day1a :: IO()
-day1a = do
-  input1 <- readFile "../input/day1.txt"
+day1a = do  -- Prints results for both part 1 and part2
+  input1 <- readFile "input/day1.txt"
   let inputs = lines input1
   let masses = map readInt inputs
   let fuels = map fuel masses
   let sumFuels = sum fuels
   let extrafuels = map extrafuel fuels
   let sumExtra = sum extrafuels
-  putStrLn "the sum of Fuel for moduels is"
+  putStrLn "the sum of fuel for modules is:"
   print sumFuels
-  putStrLn "The sum of Fuel for modules and fuel is"
+  putStrLn "The sum of fuel for modules and fuel is:"
   print $ sumExtra + sumFuels
 
 
