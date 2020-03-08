3628d395f751bc8c13326f8fc7e6bcbd02b4e6a1
Annika Rings <annika.rings@dpag.ox.ac.uk>
2020-03-08 22:45:24 +0000

    added day 16 part 2

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,28 +1,49 @@
 {-# LANGUAGE NoMonomorphismRestriction, FlexibleContexts #-}
 module Day16(
-  solution16a
+  solution16a,
+  --solution16b,
             )
   where
-import Day4 (digs)
+
 import Control.Monad
 import Data.List
 
 
-
+pattern1 :: [Int]
 pattern1  = join $ repeat [0,1,0,-1]
 
+replicateEach :: [a]->Int->[a]
 replicateEach list n = join $ map (replicate n) list
 
+replicatePattern :: Int -> [Int]
 replicatePattern  = replicateEach pattern1
 
+patterns :: [[Int]]
 patterns = map tail $ map replicatePattern [1..]
 
+zipWithx :: [Int]->[Int]->[Int]
 zipWithx = zipWith (*)
 
+multiplyWithPatterns :: [[Int]] -> [[Int]]
 multiplyWithPatterns list = zipWith zipWithx patterns list
 
+replicateList :: [Int]->[[Int]]
 replicateList list = replicate (length list) list
 
+stepFFT :: [Int] -> [Int]
 stepFFT list = map (flip mod 10 . abs . sum) $ multiplyWithPatterns $ replicateList list
 
-solution16a input = take 8 $ iterate stepFFT (digs input)!!100
+
+first7 :: String -> Int
+first7 = read . (take 7)
+
+toListOfStrings :: String -> [String]
+toListOfStrings  = map pure
+
+stringToDigits :: String -> [Int]
+stringToDigits = map read . toListOfStrings
+
+solution16a input = take 8 $ iterate stepFFT (stringToDigits input)!!100
+
+--to much memory pressure, don't run this
+solution16b input = take 8 $ drop (first7  input) $ iterate stepFFT (join (replicate 10000 (stringToDigits input))) !!100
